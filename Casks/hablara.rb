849fc31050ff7892a531eb4c610ba87da28d4196
Marc Allgeier <97551772+fidpa@users.noreply.github.com>
2026-04-13 23:36:44 +0200

    Bump hablara to 1.7.0

--- Casks/hablara.rb
+++ Casks/hablara.rb
@@ -7,8 +7,8 @@
 # ============================================================================
 
 cask "hablara" do
-  version "1.6.9"
-  sha256 "ca18d8b45b5df224ada176d82a2f33e3a3841ea488910d511ac87ef12a129c98"
+  version "1.7.0"
+  sha256 "761d7556708c645d369da67a5ca713cb32b14e66447bff41006f6363628d3110"
 
   url "https://github.com/fidpa/hablara-releases/releases/download/v#{version}/Hablara_#{version}_universal.dmg"
   name "Hablará"
